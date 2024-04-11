72d3a8d1fff7a3297bf40fbc4ea3589b0b582e00
miku-wwl <569144003@qq.com>
2024-04-12 01:13:38 +0800

    feat:handle autologin if there is a user

--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,5 +1,23 @@
+import {useUserContext} from '../hooks/contextHooks';
+
 const Profile = () => {
-  return <h2>Profile page</h2>
-}
+  // //用户保存在上下文中，只是获取用户状态
+  const {user} = useUserContext();
+  console.log('Profile:user', user);
+  return (
+    <>
+      <h2>Profile page</h2>
+      {user && (
+        <>
+          <p>Username: {user.username}</p>
+          <p>Email: {user.email}</p>
+          <p>
+            Created: {new Date(user.created_at).toLocaleDateString('zh-CN')}
+          </p>
+        </>
+      )}
+    </>
+  );
+};
 
 export default Profile;
